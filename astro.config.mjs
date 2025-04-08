0189f6c149e313d71beaa5d3d446a1755590e714
Julien <116061456+julienduflot@users.noreply.github.com>
2025-04-08 22:28:16 +0200

    build pug

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,16 @@ import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 import sitemap from '@astrojs/sitemap';
 
+const app = express();
+
+app.set('view engine', 'pug');
+
+app.get("/", async (req, res) => {
+  const records = await getPokemon();
+  res.render('accounts', { title: 'Pokemon', records });
+  //console.log(records)
+})
+
 export default defineConfig({
   site: 'https://feature-tour.netlify.app',
   integrations: [
